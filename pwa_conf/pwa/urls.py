d07c38a730b0b9278749ce0174d60f509379a54b
IanTth <ianrendrick@gmail.com>
2022-11-30 01:59:27 -0300

    rotas

--- pwa_conf/pwa/urls.py
+++ pwa_conf/pwa/urls.py
@@ -5,7 +5,18 @@ from django.conf import settings
 
 
 urlpatterns = [
-    url(r'', views.index),
-    url(r'entrar', views.entrar)
+
+    url(r'novo-ponto', views.nponto),
+    url(r'monitoramento', views.monit),
+    url(r'informacoes', views.inf),
+    url(r'dashboard', views.dash),
+    url(r'configuracoes', views.conf),
+    url(r'comparativo', views.comp),
+    url(r'camera', views.camera),
+    url(r'ajuda', views.ajuda),
+    url(r'index', views.index),
+    url(r'home', views.home),
+    url(r'entrar', views.entrar),
+    url(r'', views.index)
 
 ] + static(settings.STATIC_URL)
--- pwa_conf/pwa/views.py
+++ pwa_conf/pwa/views.py
@@ -7,3 +7,32 @@ def index(request):
 
 def entrar(request):
     return render(request,'entrar.html')
+
+def home(request):
+    return render(request,'home.html')
+
+
+
+def ajuda(request):
+    return render(request,'ajuda.html')
+
+def camera(request):
+    return render(request,'camera.html')
+
+def comp(request):
+    return render(request,'comparativo.html')
+
+def conf(request):
+    return render(request,'configuracoes.html')
+
+def dash(request):
+    return render(request,'dashboard.html')
+
+def inf(request):
+    return render(request,'informacoes.html')
+
+def monit(request):
+    return render(request,'monitoramento.html')
+
+def nponto(request):
+    return render(request,'novo-ponto.html')
